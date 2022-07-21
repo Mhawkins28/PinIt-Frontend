53f96226aefcdcee154593014d2f04cb0a59c94a
Zachary M <101447294+zach89129@users.noreply.github.com>
2022-07-20 17:41:10 -0700

    initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Project 3 BABYYYY</h1>
+    </div>
+  );
+}
+
+export default App;
