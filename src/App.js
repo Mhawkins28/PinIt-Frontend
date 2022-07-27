a2bc849b077308259175d4b3529efc7964fd1a45
Zachary M <101447294+zach89129@users.noreply.github.com>
2022-07-27 16:14:37 -0700

    changed route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home user={user} />} />
-      <Route path="/newPin" element={<NewPinForm />} />
+      <Route path="/newPin" element={<NewPin />} />
       <Route path="/login" element={<Login setUser={setUser} />} />
 
       {/* <Route
