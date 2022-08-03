fcd5f3694745593f5e7f42d5b061aab181bdd6ac
Megan Hawkins <megan.hawkins28@gmail.com>
2022-08-03 16:56:07 -0400

    got video background to work

--- src/Components/WelcomeContent/WelcomeContent.jsx
+++ src/Components/WelcomeContent/WelcomeContent.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Welcome.css'
+import Video from '../../assets/city-vid.mp4'
 
 function WelcomeContent() {
   return (
     <div className='video-container'>
-      <video src="/city-vid.mp4" loop AutoPlay />
+      <video src={Video} loop autoPlay muted />
       <div class="main">
         <h2>Get off the beaten Path </h2>
         <h1>Find Adventure</h1>
-        <p>Discover the hidden gems, wherever you are. This App is the perfect way to explore different cities in a new way </p>
+        <p>Discover hidden gems, wherever you are. This App is the perfect way to explore different cities in a new way </p>
 
         <div class="buttons">
           <button class="btn1"><a href="/login">LOGIN</a></button>
