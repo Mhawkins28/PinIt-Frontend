34f0e7fdf0976f3be5eb8635f11675feae13849b
Zachary M <101447294+zach89129@users.noreply.github.com>
2022-08-04 22:35:46 -0700

    matched map styling and centering logic

--- src/Components/Map/UserMap.jsx
+++ src/Components/Map/UserMap.jsx
@@ -10,7 +10,6 @@ import {
 import { Link } from "react-router-dom";
 import "./Map.css";
 
-
 const mapContainerStyle = {
   width: "100vw",
   height: "80vh",
@@ -45,7 +44,10 @@ const UserMap = ({
     <GoogleMap
       mapContainerStyle={mapContainerStyle}
       zoom={12}
-      center={center}
+      center={{
+        lat: JSON.parse(window.sessionStorage.getItem("lat")),
+        lng: JSON.parse(window.sessionStorage.getItem("lng")),
+      }}
       onClick={(e) => {
         setLatLng({
           lat: e.latLng.lat(),
@@ -59,53 +61,76 @@ const UserMap = ({
             featureType: "poi",
             stylers: [{ visibility: "off" }],
           },
+          { elementType: "geometry", stylers: [{ color: "#3b3c3d" }] },
+          {
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#242f3e" }],
+          },
+          { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+          {
+            featureType: "administrative.locality",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#b8845d" }],
+          },
+          {
+            featureType: "poi.park",
+            elementType: "geometry",
+            stylers: [{ color: "#263e41" }],
+          },
           {
             featureType: "road",
-            elementType: "all",
-            stylers: [
-              {
-                lightness: 20,
-              },
-            ],
+            elementType: "geometry",
+            stylers: [{ color: "#38414e" }],
+          },
+          {
+            featureType: "road",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#212a37" }],
+          },
+          {
+            featureType: "road",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#9ca5b3" }],
           },
           {
             featureType: "road.highway",
             elementType: "geometry",
-            stylers: [
-              {
-                color: "#c5c6c6",
-              },
-            ],
+            stylers: [{ color: "#746855" }],
           },
           {
-            featureType: "road.arterial",
-            elementType: "geometry",
-            stylers: [
-              {
-                color: "#e4d7c6",
-              },
-            ],
+            featureType: "road.highway",
+            elementType: "geometry.stroke",
+            stylers: [{ color: "#1f2835" }],
           },
           {
-            featureType: "road.local",
+            featureType: "road.highway",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#f3d19c" }],
+          },
+          {
+            featureType: "transit",
             elementType: "geometry",
-            stylers: [
-              {
-                color: "#fbfaf7",
-              },
-            ],
+            stylers: [{ color: "#2f3948" }],
+          },
+          {
+            featureType: "transit.station",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#d59563" }],
           },
           {
             featureType: "water",
-            elementType: "all",
-            stylers: [
-              {
-                visibility: "on",
-              },
-              {
-                color: "#acbcc9",
-              },
-            ],
+            elementType: "geometry",
+            stylers: [{ color: "#182230" }],
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.fill",
+            stylers: [{ color: "#515c6d" }],
+          },
+          {
+            featureType: "water",
+            elementType: "labels.text.stroke",
+            stylers: [{ color: "#111d2f" }],
           },
         ],
       }}
